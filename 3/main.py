cd555abd6db2643dbf767ad6ed4ecb0325606a2c
Jakerop <jakob.norin@gmail.com>
2022-12-03 17:50:09 +0100

    make find_badge dynamic

--- 3/main.py
+++ 3/main.py
@@ -11,27 +11,35 @@ def get_prio(input:str):
     return prio
 
 def find_common(items:str):
-    for item_1 in items[:int(len(items)/2)]:
-        for item_2 in items[int(len(items)/2):]:
-            if item_1 == item_2:
-                return item_1
+    midpoint = int(len(items)/2)
+    for item in items[:midpoint]:
+        if item in items[midpoint:]:
+            return item
     print("no common found")
     return
 
 def find_badge(bags:list):
     for item in bags[0]:
-        if item in bags[1] and item in bags[2]:
+        if all([item in bag for bag in bags]):
             return item
+    print("no common badge found")
+    return
 
 if __name__ == "__main__":
     prio_sum = 0
     bags = [None, None, None]
     with open(input_file) as f:
         for i, line in enumerate(f):
+            # -- part 1 --
+            # line = line.strip("\n")
+            # common_item = find_common(line)
+            # ------------
+            # -- part 2 --
             bags[i % 3] = line.strip("\n")
             if i % 3 != 2:
                 continue
             common_item = find_badge(bags)
+            # ------------
             item_prio = get_prio(common_item)
             prio_sum = prio_sum + item_prio
     print(prio_sum)
