8146433c0c0b07d7788dde451484c4490f407dea
Jakerop <119614653+Jakerop@users.noreply.github.com>
2022-12-08 13:51:16 +0100

    change variable steps_taken to trees_seen

--- 8/main.py
+++ 8/main.py
@@ -8,42 +8,43 @@ def parse_file(file_name):
         trees = [tree_row.strip("\n") for tree_row in f]
     return trees
 
-def check_top(trees, tree_height, steps_taken, x_pos, y_pos):
+def check_top(trees, tree_height, trees_seen, x_pos, y_pos):
     if y_pos == 0:
-        return [True, steps_taken]
+        return [True, trees_seen]
     elif tree_height <= trees[y_pos-1][x_pos]:
-        return [False, steps_taken+1]
+        return [False, trees_seen+1]
     else:
-        return check_top(trees, tree_height, steps_taken+1, x_pos, y_pos-1)
+        return check_top(trees, tree_height, trees_seen+1, x_pos, y_pos-1)
 
-def check_bottom(trees, tree_height, steps_taken, x_pos, y_pos):
+def check_bottom(trees, tree_height, trees_seen, x_pos, y_pos):
     if y_pos == len(trees)-1:
-        return [True, steps_taken]
+        return [True, trees_seen]
     elif tree_height <= trees[y_pos+1][x_pos]:
-        return [False, steps_taken+1]
+        return [False, trees_seen+1]
     else:
-        return check_bottom(trees, tree_height, steps_taken+1, x_pos, y_pos+1)
+        return check_bottom(trees, tree_height, trees_seen+1, x_pos, y_pos+1)
 
-def check_left(trees, tree_height, steps_taken, x_pos, y_pos):
+def check_left(trees, tree_height, trees_seen, x_pos, y_pos):
     if x_pos == 0:
-        return [True, steps_taken]
+        return [True, trees_seen]
     elif tree_height <= trees[y_pos][x_pos-1]:
-        return [False, steps_taken+1]
+        return [False, trees_seen+1]
     else:
-        return check_left(trees, tree_height, steps_taken+1, x_pos-1, y_pos)
+        return check_left(trees, tree_height, trees_seen+1, x_pos-1, y_pos)
 
-def check_right(trees, tree_height, steps_taken, x_pos, y_pos):
+def check_right(trees, tree_height, trees_seen, x_pos, y_pos):
     if x_pos == len(trees[y_pos])-1:
-        return [True, steps_taken]
+        return [True, trees_seen]
     elif tree_height <= trees[y_pos][x_pos+1]:
-        return [False, steps_taken+1]
+        return [False, trees_seen+1]
     else:
-        return check_right(trees, tree_height, steps_taken+1, x_pos+1, y_pos)
+        return check_right(trees, tree_height, trees_seen+1, x_pos+1, y_pos)
 
 if __name__ == "__main__":
     trees = parse_file(file_path)
     visible_count = 0
     best_scenic_score = 0
+    best_tree = [None, None]
     for row in range(len(trees)):
         for col in range(len(trees[row])):
             tree_height = trees[row][col]
@@ -55,6 +56,7 @@ if __name__ == "__main__":
                 visible_count += 1
             scenic_score = result_top[1]*result_bottom[1]*result_left[1]*result_right[1]
             if scenic_score > best_scenic_score:
+                best_tree = [row, col]
                 best_scenic_score = scenic_score
     print(f"visible trees: {visible_count}")
-    print(f"best scenic score: {best_scenic_score}")
+    print(f"best scenic score: {best_scenic_score}, best_tree = {best_tree}")
