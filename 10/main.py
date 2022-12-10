70bf9e4270ed583ab8745f99e352f5a39aab9792
Jakerop <jakob.norin@gmail.com>
2022-12-10 12:41:57 +0100

    correct mod operator

--- 10/main.py
+++ 10/main.py
@@ -1,4 +1,5 @@
 import os
+from math import fmod
 
 
 def get_cmd_gen():
@@ -10,39 +11,50 @@ def get_cmd_gen():
 
 
 def add_char_screen(screen:str, cycle, X):
-    sprite_position = (X+1)%40
-    if sprite_position-1 <= cycle%40 <= sprite_position+1:
+    # -1%40 = 39 and fmod(-1,40) = -1
+    sprite_position = fmod(X, 40)
+    if sprite_position-1 <= (cycle-1)%40 <= sprite_position+1:
         screen += "#"
     else:
         screen += "."
     return screen
 
 
-if __name__ == "__main__":
+def calc_signal_strength(cycle, signal_strength):
+    if (cycle-20) % 40 == 0:
+        signal_strength += cycle*X
+    return signal_strength
+
+
+def excecute_cmd(X):
     cmd_gen = get_cmd_gen()
-    signal_strength = 0
-    cycle = 0
-    X = 1
     get_next_cmd = True
-    screen = ""
     while True:
-        cycle += 1
-        screen = add_char_screen(screen, cycle, X)
-        if (cycle-20) % 40 == 0:
-            signal_strength += cycle*X
         if get_next_cmd:
-            cmd = next(cmd_gen, False)
-            if not cmd:
-                break
+            cmd = next(cmd_gen)
+            if cmd[0] == "addx":
+                get_next_cmd = False
         else:
             get_next_cmd = True
             if cmd[0] == "addx":
                 X += int(cmd[1])
-                continue
-        if cmd[0] == "noop":
-            continue
-        if cmd[0] == "addx":
-            get_next_cmd = False
+        yield X
+
+
+if __name__ == "__main__":
+    signal_strength = 0
+    cycle = 0
+    X = 1
+    screen = ""
+    excecute_cmd_gen = excecute_cmd(X)
+    while True:
+        cycle += 1
+        screen = add_char_screen(screen, cycle, X)
+        signal_strength = calc_signal_strength(cycle, signal_strength)
+        try:
+            X = next(excecute_cmd_gen)
+        except RuntimeError:
+            break
     screen = "\n".join([screen[x:x+40] for x in range(0, len(screen), 40)])
     pretty_screen = " "+" ".join([screen[x:x+1] for x in range(0, len(screen), 1)])
     print(signal_strength)
